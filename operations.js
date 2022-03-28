f7928a873bf929ec941c8ead05348809a6b554e2
Andy <andy@test.com>
2022-03-28 18:07:03 +1030

    Implemented the HTML/CSS of the calculator with buttons and screen

--- operations.js
+++ operations.js
@@ -1,3 +1,32 @@
 function add(num1, num2) {
     return num1 + num2;
 }
+
+function substract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(op, num1, num2) {
+    switch(op) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return substract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break; 
+    }
+}
